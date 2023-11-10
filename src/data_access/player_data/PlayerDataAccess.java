43d6f41a311cf5be236fafd04b74d99c817758fe
jenny-moore <jenny.moore@mail.utoronto.ca>
2023-11-10 18:15:39 -0500

    Wrote the file writer in PlayerDataAccess. This adds a new game to the PlayerData file.

---
+++ src/data_access/player_data/PlayerDataAccess.java
@@ -0,0 +1,47 @@
+package data_access.player_data;
+
+import use_case.guess.GuessDataAccessInterface;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class PlayerDataAccess implements GuessDataAccessInterface {
+    private String[] data;
+    public PlayerDataAccess() {
+        ArrayList<String> data = new ArrayList();
+        try (BufferedReader reader = new BufferedReader(new FileReader("PlayerData.csv"))) {
+            int i = 0;
+            data.set(i, reader.readLine());
+
+            String row;
+            while ((row = reader.readLine()) != null) {
+                i ++;
+                data.set(i, row);
+            }
+            data.set(i+1, "");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        this.data = (String[]) data.toArray();
+    }
+    public void saveGame(int guesses){
+        this.data[data.length - 1] = String.valueOf(guesses);
+
+        BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter("PlayerData.csv"));
+
+
+            for (String item : data) {
+                writer.write(item);
+                writer.newLine();
+            }
+            writer.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
