c1879d15d202f9eead8391bbc3d89658accace67
jenny-moore <jenny.moore@mail.utoronto.ca>
2023-11-11 14:43:45 -0500

    Wrote as much as I could for the interface adapters for the Guess use case without a ViewManagerModel and a view model for the new view.

--- src/interface_adapter/guess/GuessController.java
+++ src/interface_adapter/guess/GuessController.java
@@ -1,4 +1,17 @@
 package interface_adapter.guess;
 
+import use_case.choose_genre.ChooseInputBoundary;
+import use_case.choose_genre.ChooseInputData;
+import use_case.guess.GuessInputBoundary;
+import use_case.guess.GuessInputData;
+
 public class GuessController {
+    final GuessInputBoundary guessInteractor;
+    public GuessController(GuessInputBoundary guessInteractor){
+        this.guessInteractor = guessInteractor;
+    }
+    public void execute(String currentSong, String guess, int guesses, int maxGuesses){
+        GuessInputData data = new GuessInputData(currentSong, guess, guesses, maxGuesses);
+        guessInteractor.execute(data);
+    }
 }
--- src/interface_adapter/guess/GuessViewModel.java
+++ src/interface_adapter/guess/GuessViewModel.java
@@ -1,4 +1,37 @@
 package interface_adapter.guess;
 
-public class GuessViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class GuessViewModel extends ViewModel {
+    private final int maxGuesses = 6;
+    private GuessState guessState = new GuessState(maxGuesses);
+    public GuessViewModel() {
+        super("guess");
+    }
+    public GuessState getState(){
+        return this.guessState;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("guessState", null, this.guessState);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public void setState(GuessState guessState) {
+        this.guessState = guessState;
+    }
+
+    public int getMaxGuesses() {
+        return this.maxGuesses;
+    }
 }
