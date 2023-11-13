da9ec1593e8da16a0aaf8d86957304303ba70b12
arvin <arvin.taghinejad@mail.utoronto.ca>
2023-11-12 19:27:36 -0500

    Added the Leaderboard view presenter and fixed some issues with the viewmodel.

--- src/interface_adapter/leaderboard/LeaderboardPresenter.java
+++ src/interface_adapter/leaderboard/LeaderboardPresenter.java
@@ -13,6 +13,9 @@ public class LeaderboardPresenter implements LeaderboardOutputBoundary {
 
     @Override
     public void prepareSuccessView(LeaderboardOutputData data) {
-
+        LeaderboardState leaderboardState = leaderboardViewModel.getState();
+        leaderboardState.setData(data.getData());
+        this.leaderboardViewModel.setState(leaderboardState);
+        this.leaderboardViewModel.firePropertyChanged();
     }
 }
