d35448df4a4b923e27a8ec02aa711985b7c1f891
siqiw <siqiw.wang@mail.utoronto.ca>
2023-11-16 13:11:15 -0500

    homeview and main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,42 @@
 package app;
 
+import data_access.SpotifyPlaylistDataAccessObject;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.choose_genre.ChooseViewModel;
+import interface_adapter.play_again.PlayAgainController;
+import interface_adapter.play_again.PlayAgainPresenter;
+import use_case.play_again.PlayAgainInputBoundary;
+import use_case.play_again.PlayAgainInteractor;
+import use_case.play_again.PlayAgainOutputBoundary;
+import view.HomeView;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
-}
+    public static void main(String[] args) {
+        JFrame apep = new JFrame("Es");
+        apep.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        CardLayout cardLayout = new CardLayout();
+
+        JPanel views = new JPanel(cardLayout);
+        apep.add(views);
+
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+
+        ChooseViewModel chooseViewModel = new ChooseViewModel();
+
+        SpotifyPlaylistDataAccessObject dataAccessObject = new SpotifyPlaylistDataAccessObject();
+
+        PlayAgainOutputBoundary presenter = new PlayAgainPresenter(viewManagerModel, chooseViewModel);
+        PlayAgainInputBoundary playAgainInteractor = new PlayAgainInteractor(presenter);
+        PlayAgainController playAgainController = new PlayAgainController(playAgainInteractor);
+        HomeView homeView = new HomeView(playAgainController);
+        views.add(homeView);
+        viewManagerModel.setActiveView((homeView.viewName));
+        viewManagerModel.firePropertyChanged();
+
+        apep.setVisible(true);
+    }
+    }
+
--- src/view/HomeView.java
+++ src/view/HomeView.java
@@ -1,4 +1,53 @@
 package view;
+import interface_adapter.play_again.PlayAgainController;
 
-public class HomeView {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class HomeView extends JPanel implements ActionListener, PropertyChangeListener {
+
+    public final String viewName = "home";
+    final JButton playButton = new JButton("Start");
+
+    private final PlayAgainController playAgainController;
+
+    public HomeView(PlayAgainController playAgainController){
+
+        this.playAgainController = playAgainController;
+
+        JLabel title = new JLabel("Start");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+
+        playButton.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (e.getSource().equals(playButton)){
+
+                            playAgainController.execute();
+                        }
+                    }
+                }
+        );
+
+        this.setSize(800,600);
+        this.add(title);
+        this.add(playButton);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
 }
