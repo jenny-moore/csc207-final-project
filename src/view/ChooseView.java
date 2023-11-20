7e4138bac1fd3642c54cbad0d98d79820db0e631
jenny-moore <jenny.moore@mail.utoronto.ca>
2023-11-20 14:58:59 -0500

    Wrote the ChooseView so it updates the ChooseState when a genre is chosen and starts the game when the Choose button is pressed.

--- src/view/ChooseView.java
+++ src/view/ChooseView.java
@@ -1,6 +1,15 @@
 package view;
+import interface_adapter.choose_genre.ChooseState;
 import interface_adapter.choose_genre.ChooseViewModel;
 import interface_adapter.choose_genre.ChooseController;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 public class ChooseView extends JPanel implements ActionListener, PropertyChangeListener {
     public final String viewName = "choose";
 
@@ -10,7 +19,7 @@ public class ChooseView extends JPanel implements ActionListener, PropertyChange
     private final JButton choose;
     private final JComboBox<String> genres;
 
-    public SignupView(ChooseController controller, ChooseViewModel chooseViewModel) {
+    public ChooseView(ChooseController controller, ChooseViewModel chooseViewModel) {
         this.chooseController = controller;
         this.chooseViewModel = chooseViewModel;
 
@@ -20,10 +29,7 @@ public class ChooseView extends JPanel implements ActionListener, PropertyChange
         JLabel title = new JLabel(ChooseViewModel.TITLE_LABEL);
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        genres = newJComboBox(ChooseViewModel.GENRES);
-
-        LabelTextPanel genresInfo = new LabelTextPanel(
-                new JLabel(ChooseViewModel.GENRE_LABEL), genres);
+        genres = new JComboBox(ChooseViewModel.GENRES);
 
         JPanel buttons = new JPanel();
         choose = new JButton(ChooseViewModel.CHOOSE_BUTTON_LABEL);
@@ -31,7 +37,6 @@ public class ChooseView extends JPanel implements ActionListener, PropertyChange
 
 
         choose.addActionListener(
-                // This creates an anonymous subclass of ActionListener and instantiates it.
                 new ActionListener() {
                     public void actionPerformed(ActionEvent evt) {
                         if (evt.getSource().equals(choose)) {
@@ -45,85 +50,15 @@ public class ChooseView extends JPanel implements ActionListener, PropertyChange
                 }
         );
 
-        // TODO Add the body to the actionPerformed method of the action listener below
-        //      for the "clear" button. You'll need to write the controller before
-        //      you can complete this.
-
-        clear.addActionListener(
+        genres.addActionListener(
                 new ActionListener() {
                     @Override
-                    public void actionPerformed(ActionEvent evt) {
-                        if (evt.getSource().equals(clear)){
-                            clearController.execute();
-                        }}
-                }
-        );
-
-
-
-        cancel.addActionListener(this);
-
-        // This makes a new KeyListener implementing class, instantiates it, and
-        // makes it listen to keystrokes in the usernameInputField.
-        //
-        // Notice how it has access to instance variables in the enclosing class!
-        usernameInputField.addKeyListener(
-                new KeyListener() {
-                    @Override
-                    public void keyTyped(KeyEvent e) {
-                        SignupState currentState = signupViewModel.getState();
-                        String text = usernameInputField.getText() + e.getKeyChar();
-                        currentState.setUsername(text);
-                        signupViewModel.setState(currentState);
-                    }
-
-                    @Override
-                    public void keyPressed(KeyEvent e) {
-                    }
-
-                    @Override
-                    public void keyReleased(KeyEvent e) {
-                    }
-                });
-
-        passwordInputField.addKeyListener(
-                new KeyListener() {
-                    @Override
-                    public void keyTyped(KeyEvent e) {
-                        SignupState currentState = signupViewModel.getState();
-                        currentState.setPassword(passwordInputField.getText() + e.getKeyChar());
-                        signupViewModel.setState(currentState);
-                    }
-
-                    @Override
-                    public void keyPressed(KeyEvent e) {
-
-                    }
-
-                    @Override
-                    public void keyReleased(KeyEvent e) {
-
-                    }
-                }
-        );
-
-        repeatPasswordInputField.addKeyListener(
-                new KeyListener() {
-                    @Override
-                    public void keyTyped(KeyEvent e) {
-                        SignupState currentState = signupViewModel.getState();
-                        currentState.setRepeatPassword(repeatPasswordInputField.getText() + e.getKeyChar());
-                        signupViewModel.setState(currentState); // Hmm, is this necessary?
-                    }
-
-                    @Override
-                    public void keyPressed(KeyEvent e) {
-
-                    }
-
-                    @Override
-                    public void keyReleased(KeyEvent e) {
-
+                    public void actionPerformed(ActionEvent e) {
+                        JComboBox cb = (JComboBox)e.getSource();
+                        String genre = (String)cb.getSelectedItem();
+                        ChooseState state = chooseViewModel.getState();
+                        state.setGenre(genre);
+                        chooseViewModel.setState(state);
                     }
                 }
         );
@@ -131,16 +66,11 @@ public class ChooseView extends JPanel implements ActionListener, PropertyChange
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         this.add(title);
-        this.add(usernameInfo);
-        this.add(passwordInfo);
-        this.add(repeatPasswordInfo);
+        this.add(genres);
         this.add(buttons);
     }
 
-    /**
-     * React to a button click that results in evt.
-     */
-
+    @Override
     public void actionPerformed(ActionEvent evt) {
         JOptionPane.showConfirmDialog(this, "Cancel not implemented yet.");
     }
@@ -148,18 +78,10 @@ public class ChooseView extends JPanel implements ActionListener, PropertyChange
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         if (evt.getPropertyName().equals("state")){
-            SignupState state = (SignupState) evt.getNewValue();
-            if (state.getUsernameError() != null) {
-                JOptionPane.showMessageDialog(this, state.getUsernameError());
+            ChooseState state = (ChooseState) evt.getNewValue();
+            if (state.getError() != null) {
+                JOptionPane.showMessageDialog(this, state.getError());
             }}
-        else {
-            ClearState clearState = (ClearState) evt.getNewValue();
-            String message = new String("Deleted users: \n");
-            for (String user: clearState.getDeletedUsers()){
-                message += user + "\n";
-            }
-            JOptionPane.showMessageDialog(this, message);
-        }
     }
 
 }
