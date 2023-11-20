7c16a8f63e23d08872c7babacb9017bad578e222
jenny-moore <jenny.moore@mail.utoronto.ca>
2023-11-20 14:58:10 -0500

    Updated the ChooseState and ChooseViewModel with necessary parameters and methods for the view.

--- src/interface_adapter/choose_genre/ChooseState.java
+++ src/interface_adapter/choose_genre/ChooseState.java
@@ -2,6 +2,9 @@ package interface_adapter.choose_genre;
 
 public class ChooseState {
     private String genre;
+    private String error = "";
     public void setGenre(String genre){this.genre = genre;}
     public String getGenre(){return this.genre;}
+    public void setError(String error){this.error = error;}
+    public String getError(){return this.error;}
 }
