7643e1121643ed5395de3daa8c4b5b56d65a6fe9
mitsukakiyohara <mitsuka.kiyohara@gmail.com>
2023-11-20 12:54:13 -0500

    add initial changes

--- src/view/GameView.java
+++ src/view/GameView.java
@@ -1,4 +1,23 @@
 package view;
 
-public class GameView {
+import interface_adapter.search_bar.SearchBarViewModel;
+import view.SearchBarView;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameView extends JFrame {
+    private SearchBarView searchBarView;
+    private SearchBarViewModel searchBarViewModel;
+
+    public GameView() {
+        this.setTitle("Game View");
+        this.setSize(600, 800);
+        this.setLayout(new BorderLayout());
+        this.setResizable(false);
+
+        searchBarView = new SearchBarView(searchBarViewModel);
+        this.add(searchBarView, BorderLayout.SOUTH);
+        // Add other feature views
+    }
 }
--- src/view/SearchBarView.java
+++ src/view/SearchBarView.java
@@ -1,15 +1,71 @@
 package view;
 
 import entity.Track;
+import interface_adapter.search_bar.SearchBarViewModel;
+
+import java.beans.PropertyChangeListener;
 import java.util.List;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 // Display the output data from the presenter
-public class SearchBarView {
-    public void displaySearchResults(List<Track> tracks) {
-        // Code to update the UI with the search results
+public class SearchBarView extends JFrame{
+    private JFrame frame;
+    private JTextField searchTextField;
+    private JButton searchButton;
+    private JList<String> resultList;
+    private SearchBarViewModel viewModel;
 
+    public SearchBarView(SearchBarViewModel viewModel) {
+        this.viewModel = viewModel;
+        initializeUI();
+    }
+    
+    private void initializeUI() {
+        this.setTitle("Search Bar");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
         
+        searchTextField = new JTextField(20);
+        
+        JPanel northPanel = new JPanel(); 
+        northPanel.add(searchTextField);
 
+        this.add(northPanel, BorderLayout.NORTH);
+        this.add(new JScrollPane(resultList), BorderLayout.CENTER);
+        
+        this.pack();
+        this.setLocationRelativeTo(null); // Center on screen
+        this.setVisible(true);
+    }
+
+    private void onSearch() {
+        searchTextField.addActionListener(e -> {
+            String query = searchTextField.getText();
+            viewModel.getState().setCurrentSearchQuery(query);
+
+            // Trigger search in ViewModel
+            List<Track> tracks = viewModel.getState().getTracks();
+            updateSearchResults(tracks);
+        });
+
+        resultList.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent evt) {
+                String selectedTrack = resultList.getSelectedValue();
+                searchTextField.setText(selectedTrack);
+                // Play track here
+            }
+        });
+    }
+
+    private void updateSearchResults(List<Track> tracks) {
+        DefaultListModel<String> model = new DefaultListModel<>();
+        for (Track track : tracks) {
+            model.addElement(track.getTitle() + " - " + track.getArtist());
+        }
+        resultList.setModel(model);
     }
 
 }
