d3f734d822c4db47f8e6fc5ee4052d8edf2e8739
jenny-moore <jenny.moore@mail.utoronto.ca>
2023-11-29 14:49:05 -0500

    Changed the interactor to update the game entity with the new Track object.

--- src/use_case/choose_genre/ChooseInteractor.java
+++ src/use_case/choose_genre/ChooseInteractor.java
@@ -1,14 +1,18 @@
 package use_case.choose_genre;
 
 import entity.Game;
+import entity.Track;
+
 public class ChooseInteractor implements ChooseInputBoundary{
     final Game game = new Game();
     final ChooseOutputBoundary genrePresenter;
     final ChooseDataAccessInterface genreDataAccessObject;
+    final GameDataAccess gameDataAccess;
 
-    public ChooseInteractor(ChooseOutputBoundary genrePresenter, ChooseDataAccessInterface genreDataAccessObject){
+    public ChooseInteractor(ChooseOutputBoundary genrePresenter, ChooseDataAccessInterface genreDataAccessObject, GameDataAccess gameDataAccess){
         this.genrePresenter = genrePresenter;
         this.genreDataAccessObject = genreDataAccessObject;
+        this.gameDataAccess = gameDataAccess;
     }
     public void execute(ChooseInputData chooseGenreInputData) {
         ChooseOutputData outputData = new ChooseOutputData(chooseGenreInputData.getGenre());
@@ -20,7 +24,9 @@ public class ChooseInteractor implements ChooseInputBoundary{
             genreDataAccessObject.chooseGenre(genre, game);
         }
         try{
-        outputData.addTrack(game.generateTrack(genre));
+            Track track = game.generateTrack(genre);
+            outputData.addTrack(track);
+            gameDataAccess.getCurrgame().setCurTrack(track);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
