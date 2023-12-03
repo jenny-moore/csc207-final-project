384026308bdad3c41f58efb53da9e9a88d8e3439
arvin <arvin.taghinejad@mail.utoronto.ca>
2023-12-03 01:54:13 -0500

    Added Track and TrackFactory Tests

---
+++ test/TrackAndTrackFactoryTest.java
@@ -0,0 +1,37 @@
+import entity.Track;
+import entity.TrackFactory;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TrackAndTrackFactoryTest {
+
+    @Test
+    public void testGetArtist(){
+        TrackFactory trackFactory = new TrackFactory();
+        Track track = trackFactory.create("Lil Baby", "Low Down", "123456", "song/Lil Baby - Low Down.mp3");
+        assertEquals("Lil Baby", track.getArtist());
+    }
+
+    @Test
+    public void testGetTitle(){
+        TrackFactory trackFactory = new TrackFactory();
+        Track track = trackFactory.create("Lil Baby", "Low Down", "123456", "song/Lil Baby - Low Down.mp3");
+        assertEquals("Low Down", track.getTitle());
+    }
+
+    @Test
+    public void getSpotifyID(){
+        TrackFactory trackFactory = new TrackFactory();
+        Track track = trackFactory.create("Lil Baby", "Low Down", "123456", "song/Lil Baby - Low Down.mp3");
+        assertEquals("123456", track.getSpotifyID());
+    }
+
+    @Test
+    public void getAutoFile(){
+        TrackFactory trackFactory = new TrackFactory();
+        Track track = trackFactory.create("Lil Baby", "Low Down", "123456", "song/Lil Baby - Low Down.mp3");
+        assertEquals("song/Lil Baby - Low Down.mp3", track.getAudioFile());
+    }
+
+}
